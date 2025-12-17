7c417d4bfff7f1af8c7ca10aa90d5ff14a194fc1
kardashyanlevon1803 <kardashyanlevon1803@gmail.com>
2025-12-17 22:32:55 +0200

    Add files via upload

---
+++ lab05/lab05_1/src/app.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+
+    int a[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    int x;
+    scanf("%d", &x);
+
+    int l = 0, r = n - 1, mid, pos = -1;
+
+    while (l <= r) {
+        mid = (l + r) / 2;
+        if (a[mid] == x) {
+            pos = mid;
+            break;
+        } else if (a[mid] < x) {
+            l = mid + 1;
+        } else {
+            r = mid - 1;
+        }
+    }
+
+    printf("%d", pos);
+    return 0;
+}
