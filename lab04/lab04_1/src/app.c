c9d983fdbbcc73fa771c71048865bf5b4358eee3
kardashyanlevon1803 <kardashyanlevon1803@gmail.com>
2025-12-17 21:47:56 +0200

    Add files via upload

---
+++ lab04/lab04_1/src/app.c
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+#include <ctype.h>
+
+int main(void) {
+    char s[1000];
+    if (!fgets(s, sizeof(s), stdin)) return 0;
+    for (int i = 0; s[i]; i++)
+        s[i] = (char)toupper((unsigned char)s[i]);
+    printf("%s", s);
+    return 0;
+}
